1009d7922a8e60c31fafa428f9ba460d8911fa11
Alexander Melnikov <panches2001@gmail.com>
2023-06-26 01:06:51 +0300

    very small update

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,13 @@ import { useState } from 'react'
 
 function App() {
 
+    const [state, setState] = useState(0)
+
   return (
     <>
-
+        <div className='border bg-amber-400'>{state}</div>
+        <button className='border border-red-700 rounded-2xl' onClick={() => {setState(state + 1)}}>CLICK+</button>
+        <button className='border border-blue-600 rounded-2xl' onClick={() => {setState(state - 1)}}>CLICK-</button>
     </>
   )
 }
